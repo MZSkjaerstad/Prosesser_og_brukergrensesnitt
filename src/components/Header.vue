3ee2b9dac68d04a1a85e1f023a7a2b0082bfbb82
Your Name <you@example.com>
2022-01-19 13:07:25 +0100

    :sparkles: Made an logo component

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+	<header class="header">
+		<Logo />
+	</header>
+</template>
+
+<script>
+
+import Logo from "./Logo.vue"
+export default {
+   components: {
+		Logo
+   }
+}
+</script>
+
+<style>
+	.header {
+		width: 100%;
+		height: 4rem;
+		background: var(--background-color);
+	}
+</style>
