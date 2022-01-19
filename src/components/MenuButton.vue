7588639ce5518510a89ac12e8faa1827e2bf3d76
Your Name <you@example.com>
2022-01-19 15:45:37 +0100

    :sparkles: Created a menu button

---
+++ src/components/MenuButton.vue
@@ -0,0 +1,29 @@
+<template>
+	<button class="menu">
+		<img class="menu__image" src="./../../public/assets/images/menu.svg" alt="">
+	</button>
+</template>
+
+<script>
+
+export default {
+   components: {
+
+   }
+}
+</script>
+
+<style>
+	.menu {
+		height: 2rem;
+		background: var(--background);
+		border: 0;
+		cursor: pointer;
+		outline: none;
+	}
+
+	.menu__image  {
+		height: 100%;
+		width: auto;
+	}
+</style>
