1ff5f0b88fcaf97f5ddcab94511433701e9443c1
MZSkjaerstad <martskjae@hotmail.no>
2022-01-20 17:58:39 +0100

    :sparkles: Adding Navigation Component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,24 @@
+<template>
+   <div class="navigation">
+
+   </div>
+</template>
+
+<script>
+   export default {
+      data() {
+         return {
+
+         }
+      }
+   }
+</script>
+
+<style>
+	.navigation {
+		display: flex;
+		width: 100%;
+		height: 4rem;
+		background: var(--secondary-color);
+	}
+</style>
