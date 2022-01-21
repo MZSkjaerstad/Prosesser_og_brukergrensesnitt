432d9a1cfe45811a7d3a85bd7d489d8b8f4be634
Your Name <you@example.com>
2022-01-21 10:22:21 +0100

    :lipstick: Header UI updated

---
+++ src/components/Header/MenuButton.vue
@@ -0,0 +1,29 @@
+<template>
+	<button class="menu">
+		<img class="menu__image" src="./../../../public/assets/images/menu.svg" alt="">
+	</button>
+</template>
+
+<script>
+
+export default {
+   components: {
+
+   }
+}
+</script>
+
+<style>
+	.menu {
+		height: 1rem;
+		background: inherit;
+		border: 0;
+		cursor: pointer;
+		outline: none;
+	}
+
+	.menu__image  {
+		height: 100%;
+		width: auto;
+	}
+</style>
