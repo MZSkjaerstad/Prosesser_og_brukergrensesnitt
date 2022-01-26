0dfa91a1eb3470667ddb58de3277b360089bc457
Your Name <you@example.com>
2022-01-26 21:59:37 +0100

    :lipstick: Style and small interaction changes to the header

---
+++ src/components/SubscribePopup.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="subscribe">
+	  <p>Subscribe to the in(dex) newsletter for monthly updates</p>
+  </div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+
+		}
+	}
+}
+</script>
+
+<style>
+	.subscribe {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		padding: 0 5rem;
+		font-size: 3rem;
+		position: sticky;
+		bottom: 0;
+	}
+</style>
