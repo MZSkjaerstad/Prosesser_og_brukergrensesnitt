6851fd46697734b8b543bc2bbe36985761163d7f
Your Name <you@example.com>
2022-01-26 17:06:54 +0100

    :art: Store restructured and header is now fixed to the top of the screen

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import Vuex from 'vuex'
+ import Vuex from 'vuex'
 import {store as articles} from './modules/articles/store'
 import {store as navigation} from './modules/navigation/store'
 import {store as pages} from './modules/pages/store'
