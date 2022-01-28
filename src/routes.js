a0009eee2b902a97baf1a2d6c2d5215c8b071a6f
Your Name <you@example.com>
2022-01-28 07:07:37 +0100

    :ambulance: Small changes to the paths and deleted dummy data

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,22 @@
 import Home from './views/Home.vue';
 import About from './views/AboutUs.vue';
-import Dummy from './views/Dummy.vue';
 import ArticleByKrzysztof from './views/ArticleByKrzysztof.vue';
 import ArticleByDaniel from './views/ArticleByDaniel.vue';
 import ArticleByJimmyDurham from './views/ArticleByJimmyDurham.vue';
 
 export default [
    { name: 'home', path: '/', component: Home },
-   { name: 'about', path: '/about', component: About },
    { name: 'about-us', path: '/about-us', component: About },
-   { name: 'announcement', path: '/announcement', component: Dummy, props: { name: "Announcement" }},
-   { name: 'journal', path: '/journal', component: Dummy, props: { name: "Journals" }},
-   { name: 'architecture', path: '/architecture', component: Dummy, props: { name: "Architecture" }},
-   { name: 'video&film', path: '/video&film', component: Dummy, props: { name: "Video & Film" }},
-   { name: 'live', path: '/live', component: Dummy, props: { name: "Live" }},
-   { name: 'notes', path: '/notes', component: Dummy, props: { name: "Notes" }},
-   { name: 'books', path: '/books', component: Dummy, props: { name: "Books" }},
-   { name: 'projects', path: '/projects', component: Dummy, props: { name: "projects" }},
-   { name: 'podcasts', path: '/podcasts', component: Dummy, props: { name: "podcasts" }},
+   { name: 'announcement', path: '/announcement'},
+   { name: 'journal', path: '/journal'},
+   { name: 'architecture', path: '/architecture'},
+   { name: 'video&film', path: '/video&film'},
+   { name: 'live', path: '/live'},
+   { name: 'notes', path: '/notes'},
+   { name: 'books', path: '/books'},
+   { name: 'projects', path: '/projects'},
+   { name: 'podcasts', path: '/podcasts'},
    { name: 'mutual-aid-social-distancing-and-dual-power-in-the-state-of-emergency', path: '/mutual-aid-social-distancing-and-dual-power-in-the-state-of-emergency', component: ArticleByDaniel },
    { name: 'the-institute-of-the-cosmos', path: '/the-institute-of-the-cosmos', component: ArticleByKrzysztof },
    { name: 'jimmie-durham', path: '/jimmie-durham', component: ArticleByJimmyDurham },
+]
