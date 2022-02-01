31cf0970bdf4b497f66f518dfc27d50a6dd5e510
Felix <55444462+foxy773@users.noreply.github.com>
2022-02-01 11:24:43 +0100

    Revert "Mobile responsive"

--- src/components/SubscribePopup/SubscribePopup.vue
+++ src/components/SubscribePopup/SubscribePopup.vue
@@ -1,11 +1,9 @@
 <template>
-	<div class="grid">
-  		<div class="subscribe" v-if='this.SubscribePopup.visible' @click='showInput()'>
-	  		<p class="subscribe__text" v-if='!this.SubscribeInput.visible'>Subscribe to the in(dex) newsletter for monthly updates</p>
-	  		<SubscribeInput v-if='this.SubscribeInput.visible' />
-	  		<CloseButton class="subscribe__close" @click="closePopup()" />
-		</div>
-	</div>
+  <div class="subscribe" v-if='this.SubscribePopup.visible' @click='showInput()'>
+	  <p class="subscribe__text" v-if='!this.SubscribeInput.visible'>Subscribe to the in(dex) newsletter for monthly updates</p>
+	  <SubscribeInput v-if='this.SubscribeInput.visible' />
+	  <CloseButton class="subscribe__close" @click="closePopup()" />
+  </div>
 </template>
 
 <script>
@@ -47,49 +45,37 @@ export default {
 </script>
 
 <style>
-	.grid {
-		display: grid;
-		grid-template-columns: repeat(12, 1fr);
-		z-index: 1;
-		position: sticky;
-		bottom: 0;
-		padding: var(--spacing-small);
-	}
-
 	.subscribe {
-		display: grid;
-		grid-column: 1 / span 12;
-		grid-row: 1;
-		grid-template-columns: inherit;
+		display: flex;
 		flex-direction: row;
+		justify-content: center;
 		align-items: center;
+		margin: auto;
+		width: 95%;
 		height: 62px;
-		width: 100%;
+		padding: 11px;
 		font-family: var(--font-family);
 		font-weight: var(--font-weight-body);
 		font-size: var(--font-size-body);
-
+		position: sticky;
+		bottom: 10px;
 		background: var(--secondary-color);
 		cursor: pointer;
+		z-index: 1;
 		border-radius: 10px;
-	}
-
-	.subscribe__text {
-		overflow: hidden;
-		width: 100%;
-		grid-column: 3 / span 8;
-		grid-row: 1;
-		text-align: center;
 	}
 
 	.subscribe:hover {
 		background: var(--highlight-color);
 	}
 
+	.subscribe__text {
+
+	}
+
 	.subscribe__close {
-		padding-left: var(--spacing-medium);
-		grid-column: 12 / span 1;
-		grid-row: 1;
+		position: absolute;
+		right: 0;
 	}
 
 </style>
